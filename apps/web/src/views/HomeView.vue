2283e0f0270ece7e3cd6a3291a685b3db095b4ea
Fabrício Paz Costa <fpcportodigital@gmail.com>
2026-07-08 09:58:53 -0300

    feat: bootstrap HireFlow project

---
+++ apps/web/src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <h1>🚀 HireFlow</h1>
+    <p>O futuro da sua carreira começa aqui.</p>
+  </main>
+</template>
+
+<style scoped>
+main {
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+h1 {
+  font-size: 3rem;
+}
+
+p {
+  font-size: 1.2rem;
+}
+</style>
